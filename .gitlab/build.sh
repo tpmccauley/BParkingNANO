3ae120c4c70202ce62b6923a5a50d2ce5fa4cd34
Tom McCauley <tpmccauley@gmail.com>
2020-10-20 13:37:41 +0100

    fixes

--- .gitlab/build.sh
+++ .gitlab/build.sh
@@ -11,12 +11,12 @@ git cms-init --upstream-only
 
 echo -e "/.gitignore \n /.clang-tidy \n /.clang-format \n /RecoEgamma/EgammaElectronProducers/ \n /RecoEgamma/EgammaTools/ \n /RecoEgamma/ElectronIdentification/ \n" > .git/info/sparse-checkout
 
-git remote add crovelli git@github.com:crovelli/cmssw.git
+git remote add crovelli https://github.com/crovelli/cmssw.git
 git fetch crovelli
 git checkout -b from-CMSSW_10_2_15__ID-2020Jul26-depth13-700__WithFinalReg crovelli/from-CMSSW_10_2_15__ID-2020Jul26-depth13-700__WithFinalReg
 git cms-addpkg TrackingTools/TransientTrack
 git cms-merge-topic -u CMSBParking:GsfTransientTracks
 git cms-merge-topic -u CMSBParking:fixKinParticleVtxFitter
-git clone git@github.com:CMSBParking/BParkingNANO.git  ./PhysicsTools
+ls -R ${CI_PROJECT_DIR}
 git cms-addpkg PhysicsTools/NanoAOD
 scram b
